8d7cb3f33828ad84b96bdc1d68aa3e52063b385a
Erickssom Melgaco de Oliveira <erickssommelgaco@gmail.com>
2024-05-30 21:54:12 -0300

    listagem de pokemons em javascript

--- assets/.js/main.js
+++ assets/.js/main.js
@@ -1,1 +1,17 @@
-console.log("Sucess")
+
+const offset= 0
+const limit =10
+const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${offset}'
+
+
+
+fetch(url)
+.then(function(response) {
+    console.log(response)
+})
+.catch(function(error) {
+    console.error(error)
+})
+.finally(function() {
+    console.log('Requisição concluida')
+})
