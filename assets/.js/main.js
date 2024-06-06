b53777cde0e688942e4740e2faf150fad0593c57
Erickssom Melgaco de Oliveira <erickssommelgaco@gmail.com>
2024-06-06 15:48:43 -0300

    adicionando uma lista para os pokemons no js (esta vermelho mas esta funcionando)

--- assets/.js/main.js
+++ assets/.js/main.js
@@ -3,14 +3,34 @@ const offset= 0
 const limit =10
 const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${offset}'
 
+function convertPokemonToHtml(pokemon) {
+   return '
+          <li class="pokemon">
+              <span class="number">#001</span>
+              <spam class="name">$`{pokemon.name}</span>
+              
+                <div class="detail">
+                     <ol class="types">
+                     <li class="type">grass</li>
+                    <li class="type">poison</li>
+                  </ol>
 
+                  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+                        alt="$`{pokemon.name}">
+                </div>
+            </li>
+ '      
+}
 
 fetch(url)
    .then((response) => response.json())
    .then((jsonBody) => jsonBody.results)
    .then((pokemonList) => {
-      debugger
-       console.log(pokemonList)
+      
+       for (let i = 0; i < pokemonList.lengt; i++) { 
+         const pokemon = pokemonList[i];
+         console.log(convertPokemonToLi(pokemon))
+      }
    })
    .catch((error)=>console.error(error))
  
