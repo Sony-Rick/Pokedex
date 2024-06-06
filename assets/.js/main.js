966f4781673690498a40def105c0931de88f848f
Erickssom Melgaco de Oliveira <erickssommelgaco@gmail.com>
2024-06-05 21:09:44 -0300

    deixando a resposta da promese elegante

--- assets/.js/main.js
+++ assets/.js/main.js
@@ -6,15 +6,7 @@ const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${offset}'
 
 
 fetch(url)
-  .then((response)=> {
-    return response.json()
-  })
-  .then(function(jsonBody) {
-    console.log(jsonBody)   
-  })
-  .catch(function(error) {
-    console.error(error)
-  })
-  .finally(function() {
-    console.log('Requisição concluida!')
-  })
+  .then((response)=> response.json())
+  .then((jsonBody)=>console.log(jsonBody))
+  .catch((error)=>console.error(error))
+ 
