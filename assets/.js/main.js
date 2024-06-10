9a9e7561eaae929fce11e880d7b7ea30fc7df9d0
Erickssom Melgaco de Oliveira <erickssommelgaco@gmail.com>
2024-06-10 16:19:06 -0300

    tentando finalizar a lista, não esta funcionando

--- assets/.js/main.js
+++ assets/.js/main.js
@@ -7,7 +7,7 @@ function convertPokemonToHtml(pokemon) {
    return '
           <li class="pokemon">
               <span class="number">#001</span>
-              <spam class="name">$`{pokemon.name}</span>
+              <spam class="name">$`{pokemon.name}</spam>
               
                 <div class="detail">
                      <ol class="types">
@@ -15,23 +15,22 @@ function convertPokemonToHtml(pokemon) {
                     <li class="type">poison</li>
                   </ol>
 
-                  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
-                        alt="$`{pokemon.name}">
+                  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                        alt="$`{pokemon.name}" >
                 </div>
-            </li>
- '      
+            </li>'
+   
 }
+
+const pokemonList = document.getElementById('pokemonList')
 
 fetch(url)
    .then((response) => response.json())
    .then((jsonBody) => jsonBody.results)
-   .then((pokemonList) => {
-      
-       for (let i = 0; i < pokemonList.lengt; i++) { 
-         const pokemon = pokemonList[i];
-         console.log(convertPokemonToLi(pokemon))
-
-         document.getElementsByClassName('pokemons')
+   .then((pokemons) => {
+      for (let i = 0; i < pokemonList.lengt; i++) { 
+            const pokemon = pokemonList[i];
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)
       }
    })
    .catch((error)=>console.error(error))
