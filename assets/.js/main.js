e84846d4fab21e81ce4adfad33d37e0dba9c4e4c
Erickssom Melgaco de Oliveira <erickssommelgaco@gmail.com>
2024-06-12 16:13:53 -0300

    map funcionando

--- assets/.js/main.js
+++ assets/.js/main.js
@@ -1,6 +1,4 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
 
 function convertPokemonToLi(pokemon) {
     return `
@@ -24,8 +22,6 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-pokeApi.getPokemons().then((pokemons =>[]) => {
-    pokemonList.innerHTML +=  pokemons.map(convertPokemonToLi).join('') 
-
-  })
-   
+pokeApi.getPokemons().then(pokemons => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
+});
